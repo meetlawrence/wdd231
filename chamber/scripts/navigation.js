3104e890a80256b43afdd5a83e9d2f584f2337cf
rivrjord <lokon4@byupathway.edu>
2026-01-10 18:46:37 +0100

    Testing

--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -1,7 +1,19 @@
-const mainnav = document.querySelector('.navigation ul');
-const hambutton = document.querySelector('#menu');
+/* SELECT ELEMENTS */
+const mainNav = document.querySelector('.navigation ul');
+const hamButton = document.querySelector('#menu');
+const navLinks = document.querySelectorAll('.navigation a');
 
-hambutton.addEventListener('click', () => {
-    mainnav.classList.toggle('open');
-    hambutton.classList.toggle('open');
+/* TOGGLE MENU */
+hamButton.addEventListener('click', () => {
+    mainNav.classList.toggle('open');
+    hamButton.classList.toggle('open');
+    document.body.style.overflow = mainNav.classList.contains('open') ? 'hidden' : 'auto';
+});
+
+/* CLOSE MENU WHEN A LINK IS CLICKED */
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        mainNav.classList.remove('open');
+        hamButton.classList.remove('open');
+    });
 });
