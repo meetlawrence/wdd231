9b65072134b584a97aec25fbf7de831adafbb794
rivrjord <lokon4@byupathway.edu>
2026-01-10 14:25:31 +0100

    Testing

---
+++ chamber/scripts/navigation.js
@@ -0,0 +1,7 @@
+const mainnav = document.querySelector('.navigation ul');
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('open');
+    hambutton.classList.toggle('open');
+});
