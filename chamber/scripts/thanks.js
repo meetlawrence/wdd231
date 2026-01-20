9c244686dfb2738553e51203af0f9e34e8c00a3a
meetlawrence <lokon4@byupathway.edu>
2026-01-20 22:27:58 +0100

    Made modifications

---
+++ chamber/scripts/thanks.js
@@ -0,0 +1,23 @@
+const getString = window.location.search;
+console.log(getString);
+
+const myInfo = new URLSearchParams(getString);
+console.log(myInfo);
+
+// console.log(myInfo.get("first"));
+// console.log(myInfo.get("last"));
+// console.log(myInfo.get("phone"));
+// console.log(myInfo.get("email"));
+// console.log(myInfo.get("ordinance"));
+// console.log(myInfo.get("date"));
+// console.log(myInfo.get("location"));
+
+document.querySelector("#results").innerHTML = `
+<p><strong>Registrant:</strong> <span>${myInfo.get("first")} ${myInfo.get("last")}</span></p>
+<p><strong>Title:</strong> <span>${myInfo.get("org-title")}</span></p>
+<p><strong>Email:</strong> <span>${myInfo.get("email")}</span></p>
+<p><strong>Phone:</strong> <span>${myInfo.get("phone")}</span></p>
+<p><strong>Organization:</strong> <span>${myInfo.get("organization")}</span></p>
+<p><strong>Membership:</strong> <span style="text-transform: uppercase; color: var(--accent1); font-weight: bold;">${myInfo.get("membership")}</span></p>
+<p><strong>Description:</strong> <span>${myInfo.get("description")}</span></p>
+`
