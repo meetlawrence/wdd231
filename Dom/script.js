40d301b488e4c622b1451a22c0f349313c97f72a
meetlawrence <lokon4@byupathway.edu>
2026-02-12 09:46:05 +0100

    Made modifications

---
+++ Dom/script.js
@@ -0,0 +1,21 @@
+const link = document.querySelector("a");
+
+link.textContent = "Mozilla Developer Network";
+
+const sect = document.querySelector("section");
+const para = document.createElement("p");
+para.textContent = "We hope you enjoyed the ride.";
+
+sect.appendChild(para);
+
+
+const text = document.createTextNode(
+    " — the premier source for web development knowledge.",
+);
+
+const linkPara = document.querySelector("p");
+linkPara.appendChild(text);
+
+sect.appendChild(linkPara);
+
+para.classList.add("highlight");
